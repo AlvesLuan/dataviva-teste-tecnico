4a3e8500ba7edef293c2c00578bea9bc4df769a9
Luan K. Alves <luankauan10@gmail.com>
2026-02-04 15:06:37 -0300

    4. Validação de Parênteses, updated

--- desafios/desafio4.py
+++ desafios/desafio4.py
@@ -1,37 +1,41 @@
-conjunto = str(input("Validação de Parênteses\nDigite sua string: "))
-vetor = list(conjunto)
- 
-#De cara, se não for par alguma coisa ta errada
-if (len(vetor) % 2 != 0):
-    print("❌ Inválido (faltou fechar)")
+def Validacao_de_Parenteses(conjunto):
+    vetor = list(conjunto)
 
-else:
-    validador = True
-    
-    pilha = []
+    #De cara, se não for par alguma coisa ta errada
+    if (len(vetor) % 2 != 0):
+        print("❌ Inválido (faltou fechar)")
 
-    # dicionario relação chave -> valor
-    pares = {
-        ')': '(',
-        ']': '[',
-        '}': '{'
-    } 
-
-    for simboloAtual in vetor:
-        if simboloAtual in '([{':
-            pilha.append(simboloAtual)
-
-        elif simboloAtual in ')]}':
-            #verifica se a pilha ta vazia ou se o topo da pilha é igual ao seu par
-            if not pilha or pilha.pop() != pares[simboloAtual]:
-                validador = False
-                break
-
-    #sobrou na pilha, ent tá errado
-    if pilha:
-        validador = False
-
-    if validador:
-        print("✅ Válido")
     else:
-        print("❌ Inválido")
+        validador = True
+        
+        pilha = []
+
+        # dicionario relação chave -> valor
+        pares = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        } 
+
+        for simboloAtual in vetor:
+            if simboloAtual in '([{':
+                pilha.append(simboloAtual)
+
+            elif simboloAtual in ')]}':
+                #verifica se a pilha ta vazia ou se o topo da pilha é igual ao seu par
+                if not pilha or pilha.pop() != pares[simboloAtual]:
+                    validador = False
+                    break
+
+        #sobrou na pilha, ent tá errado
+        if pilha:
+            validador = False
+
+        if validador:
+            print("✅ Válido")
+        else:
+            print("❌ Inválido")
+
+
+a_ser_validado = str(input("Validação de Parênteses\nDigite sua string: "))
+Validacao_de_Parenteses(a_ser_validado)
