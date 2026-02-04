248f63b9c0d9b6200762aad3a6593c5fb43e2f6f
Luan K. Alves <luankauan10@gmail.com>
2026-02-04 12:03:02 -0300

    Update desafio1.py

--- desafios/desafio1.py
+++ desafios/desafio1.py
@@ -1,1 +1,10 @@
-teste
+for i in range(1, 101):
+    tipo = ""
+    if i % 3 == 0:
+        tipo += "Fizz"
+    if i % 5 == 0:
+        tipo += "Buzz"
+
+    print(i, tipo) #estrategia de formatar meu print
+    
+    #quebra de linha do python***
