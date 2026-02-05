6bbffd8cd90799212e805428cd507f939ff178d1
Luan K. Alves <luankauan10@gmail.com>
2026-02-05 02:05:07 -0300

    3. Encontrar Duplicados, updated

--- desafios/desafio3.py
+++ desafios/desafio3.py
@@ -1,15 +1,15 @@
 def duplicados(lista):
     #cria 2 hashsets, uma pra todos os numeros e outra apenas pra duplicados
     numeros = set()
-    duplicados = set()  
-
+    repetidos = set()  
+    
     #comparo o elemento atual com a set de numeros que esta sendo alimntada por esse for
     for num_atual in lista:
         if num_atual in numeros:
-            duplicados.add(num_atual)
+            repetidos.add(num_atual)
         else:
             numeros.add(num_atual)
 
-    return list(duplicados)
+    return list(repetidos)
 
 print(duplicados([1, 2, 3, 2, 5]))
