b9bb3bc1ef94931c871758ec1948527727b7fc31
Luan K. Santos <luankauan10@gmail.com>
2026-02-07 13:10:24 -0300

    2. Verificador de Palíndromo, updated

--- desafios/desafio2.py
+++ desafios/desafio2.py
@@ -1,4 +1,7 @@
 def e_palindromo(palavra):
+    #coloco a palavra enviada em minusculo pra evitar erro, apenas.
+    palavra = palavra.lower()
+
     #apenas comparo com a palavra inversa usando slicing
     if palavra == palavra[::-1]: 
         return True
